9bd1e47ec1acf018a5e6874d3514594a458574c9
hamon1 <leejh021122@naver.com>
2024-11-04 17:12:59 +0900

    avatar movement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React,{ useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../contexts/UserContext';
+import '../App.css';
 
 // import { useNavigate } from 'react-router-dom';
 import { useNavigation } from '../utils/navigate';
@@ -11,10 +12,19 @@ const Home = () => {
     // const navigate = useNavigate();
     const { user, setUser } = useContext(UserContext);
 
+    const [position, setPosition] = useState({ x: 50, y: 50 });
     
     const handleNavigate = () => {
         // navigate('/customize');
         goToCustomize(); // useNavigation Hook instead of useNavigate()
+    }
+
+    const handleClick = (event) => {
+        const container = event.currentTarget.getBoundingClientRect();
+
+        const x = event.clientX;
+        const y = event.clientY - container.top;
+        setPosition({ x, y });
     }
 
     return (
@@ -23,9 +33,16 @@ const Home = () => {
             <h1>Wellcome Home!</h1>
             <button onClick={handleNavigate}>커스텀!</button>
             <dic class="room-space">
-                <div class="moveable-space">
+                <div class="movement-space" onClick={handleClick}>
                     <div class="avatar-box">
-                        <p>avartar</p>
+                        <div 
+                            className="avatar"
+                            style={{
+                                transform: `translate(${position.x}px, ${position.y}px)`,
+                            }}
+                            >
+                            <p>avartar</p>
+                        </div>
                         <p>username: {user.username}</p>
                     </div>
                 </div>
