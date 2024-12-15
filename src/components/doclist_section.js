1ede4d7030b48c16e5de4e6e40a0ff3bd5f10bf1
hamon1 <leejh021122@naver.com>
2024-12-16 02:01:36 +0900

    dragLayer 추가

--- src/components/doclist_section.js
+++ src/components/doclist_section.js
@@ -1,19 +1,52 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import { useNavigation } from '../utils/navigate';
 
 import { api, deleteDoc } from '../api';
 
 import Swal from "sweetalert2";
 
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { DndProvider, useDrag, useDrop, useDragLayer } from 'react-dnd';
 
+const CustomDragLayer = () => {
+    const { isDragging, currentOffset } = useDragLayer((monitor) => ({
+        isDragging: monitor.isDragging(),
+        currentOffset: monitor.getSourceClientOffset(),
+    }));
+
+    if (!isDragging || !currentOffset) {
+        return null;
+    }
+
+    return (
+        <div
+            style={{
+                position: 'fixed',
+                pointerEvents: 'none',
+                left: currentOffset.x,
+                top: currentOffset.y,
+                transform: 'translate(-50%, -50%)', // 드래그 아이템을 마우스 중앙에 위치
+                zIndex: 1000,
+                backgroundColor: 'rgba(0,0,0,0.8)',
+                color: 'white',
+                padding: '10px',
+                borderRadius: '5px',
+            }}
+        >
+            드래그 중...
+        </div>
+    )
+}
 
 const Doclist_section = ({key, data, onDelete}) => {
     const [isDraggable, setIsDraggable] = useState(false);
+    const [position, setPosition] = useState({x: -100, y: -100});
+
     const { goToDocview } = useNavigation();
 
     const ref = useRef(null);
     const timeOutRef = useRef(null);
+
+    const dragPreview = useRef(null);
 
     const [, dragRef, previewRef] = useDrag({
         type: 'DOC',
@@ -21,14 +54,47 @@ const Doclist_section = ({key, data, onDelete}) => {
         canDrag: () => isDraggable,
     })
 
+    // useEffect(() => {
+    //     console.log('useEffect[drag]: ', isDraggable, position.x);
+    //     const updateMousePosition = (event) => {
+    //         console.log('updateMousePosition', position.x);
+    //         // setPosition({x: event.clientX, y: event.clientY});
+    //         if (isDraggable) {
+    //             setPosition({ x: event.clientX, y: event.clientY });
+    //         }
+    //     };
+    //     // if (isDraggable) {
+    //         // 드래그 가능 상태일 때 이벤트 리스너 추가
+    //         window.addEventListener('mousemove', updateMousePosition);
+    //     // } else {
+    //         // 드래그 가능 상태가 아닐 때 기존 리스너 제거
+    //         // window.removeEventListener('mousemove', updateMousePosition);
+    //     // }
     
-    const handleMouseDown = () => {
+    //     // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    //     return () => {
+    //         window.removeEventListener('mousemove', updateMousePosition);
+    //     };
+    // },[isDraggable])
+
+    
+    const handleMouseDown = (event) => {
         console.log('handleMouseDown');
+        setPosition({ x: event.clientX, y: event.clientY });
+        
         timeOutRef.current = setTimeout(() => {
             setIsDraggable(true);
             console.log('isDraggable: ', isDraggable);
         }, 500);
     }
+
+    // const handleMouseMove = (event) => {
+    //     // if (!isDraggable) return;
+    //     console.log('handleMouseMove');
+
+    //     setPosition({ x: event.clientX, y: event.clientY });
+    // }
+
     const handleMouseUp = () => {
         console.log('handleMouseUp');
         clearTimeout(timeOutRef.current);
@@ -36,7 +102,8 @@ const Doclist_section = ({key, data, onDelete}) => {
     }
     
     dragRef(ref);
-    previewRef(ref);
+    previewRef(dragPreview);
+
     const handleClick = (docId) => {
         console.log('Document ID:', docId);
         goToDocview(docId);
@@ -88,12 +155,14 @@ const Doclist_section = ({key, data, onDelete}) => {
     // }
 
     return (
+        <>
         <div 
             class="book" 
             ref={ref}
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
             onMouseLeave={handleMouseUp}
+            // onMouseMove={handleMouseMove}
         >
             <div class="cover">
                 {/* <img src="book_cover.jpg" alt="book cover" /> */}
@@ -106,6 +175,25 @@ const Doclist_section = ({key, data, onDelete}) => {
                 <button class="delete library-btn button" onClick={handleDelete}>삭제</button>
             </div>
         </div>
+        <CustomDragLayer/>
+        {/* <div ref={dragPreview} style={{ position: 'absolute' }}>///preview///</div> */}
+        {/* {isDraggable? (
+            <>
+                <div style={{ 
+                    position: 'fixed',
+                    top: position.y,
+                    left: position.x,
+                    zIndex: 1000,
+                    backgroundColor: 'rgba(0,0,0,0.8)',
+                    color: 'white',
+                    padding: '10px',
+                    // transform: `translate(${position.x}px, ${position.y}px)`,
+                    }}> 마우스 위치: X: {position.x}, Y: {position.y} </div>
+            </>
+        ):
+        <div></div>
+        } */}
+        </>
     )
 }
 export default Doclist_section;
